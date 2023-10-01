a66bb835c4213d63b48b8b62776a7db905c853c7
laragercek <laragercek@gmail.com>
2023-10-01 14:42:49 +1100

    add content styling and hello button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
-function greet() {
+function sayHello() {
     alert('Hello, welcome to my personal website!');
 }
