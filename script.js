c423cd4418457f392738d4e30d461e7a45c4e746
laragercek <laragercek@gmail.com>
2023-10-01 17:46:09 +1100

    add js event listeners and ingredients button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function sayHello() {
     alert('Hello, welcome to my personal website!');
 }
+
+// Toggle display of ingredients 
+document.getElementById('toggle-ingredients').addEventListener('click', () => {
+    let ingredientList = document.getElementById('ingredient-list');
+    if (ingredientList.style.display === 'none' || ingredientList.style.display === '') {
+        ingredientList.style.display = 'block';
+    } else {
+        ingredientList.style.display = 'none';
+    }
+});
